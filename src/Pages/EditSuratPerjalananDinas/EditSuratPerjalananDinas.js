6d9fd1cc353e0ee602d4f09ede78bfddfb56eea7
Darell Mona <s2200064@student.unklab.ac.id>
2023-11-22 12:09:27 +0800

    stall: wait for api

--- src/Pages/EditSuratPerjalananDinas/EditSuratPerjalananDinas.js
+++ src/Pages/EditSuratPerjalananDinas/EditSuratPerjalananDinas.js
@@ -1,4 +1,4 @@
-import { Fragment, useRef } from "react";
+import { Fragment, useEffect, useRef, useState } from "react";
 import {
   Card,
   CardBody,
@@ -12,37 +12,46 @@ import Select from "react-select";
 import Breadcrumbs from "../../components/Common/Breadcrumb";
 import Flatpickr from "react-flatpickr";
 import DISTRICT from "@regions-of-indonesia/data/district";
-import PROVINCE from "@regions-of-indonesia/data/province";
+import { Form, Formik } from "formik";
+import axios from "axios";
+import { capitalizeString } from "../../Utility";
+import * as Yup from "yup";
+
+const validationSchema = Yup.object().shape({
+  commitmentMaker: Yup.string().required("PPB harus dipilih"),
+  placeOfDeparture: Yup.string().required("Tempat berangkat harus dipilih"),
+  destination: Yup.array().min(1, "Pilih minimal 1 tujuan"),
+  dateOfLetter: Yup.string().required("Tanggal surat harus diisi"),
+});
 
 const EditSuratPerjalananDinas = () => {
+  const [employees, setEmployees] = useState([]);
+  const [checked, setChecked] = useState(false);
   const datePickerRef = useRef(null);
 
-  console.log(DISTRICT);
-  console.log(PROVINCE);
-
-  const filteredDistricts = [
-    {
-      options: Object.keys(DISTRICT)
-        .filter((key) => key.startsWith("71"))
-        .map((key) => ({
-          value: key,
-          label: DISTRICT[key],
-        })),
-    },
-  ];
-
-  // const filteredDistricts = [];
-
-  // Iterate over the data and filter regions with 71 data
-  // for (const key in data) {
-  //   if (data.hasOwnProperty(key)) {
-  //     if (key.startsWith("71.")) {
-  //       filteredDistricts.push(data[key]);
-  //     }
-  //   }
-  // }
+  const filteredDistricts = Object.keys(DISTRICT)
+    .filter((key) => key.startsWith("71"))
+    .map((key) => ({
+      value: DISTRICT[key],
+      label: DISTRICT[key],
+    }));
 
   console.log(filteredDistricts);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:2000/employees")
+      .then((res) => {
+        const normalized = res.map((employee) => ({
+          value: employee.name,
+          label: employee.name,
+        }));
+        setEmployees(normalized);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
   return (
     <Fragment>
@@ -52,105 +61,170 @@ const EditSuratPerjalananDinas = () => {
             title="Surat Tugas"
             breadcrumbItem="Edit Surat Perjalanan Dinas"
           />
-          <Card>
-            <CardBody>
-              <CardTitle>PEJABAT PEMBUAT KOMITMEN</CardTitle>
-              <CardBody className="p-0 px-4">
-                <div className="mb-3">
-                  {/* <Label>Warna Bar</Label> */}
-                  <Select
-                    // value={selectedGroup}
-                    // onChange={() => {
-                    //   handleSelectGroup();
-                    // }}
-                    // options={warnaOption}
-                    classNamePrefix="select2-selection"
-                  />
-                </div>
-              </CardBody>
-            </CardBody>
-            <CardBody className="pt-0">
-              <CardTitle>KEBERANGKATAN & DESTINASI</CardTitle>
-              <CardBody className="p-0 px-4">
-                <div className="mb-3">
-                  <Label>Tempat Berangkat</Label>
-                  <Select
-                    // value={selectedGroup}
-                    // onChange={() => {
-                    //   handleSelectGroup();
-                    // }}
-                    // options={warnaOption}
-                    classNamePrefix="select2-selection"
-                  />
-                </div>
-                <div className="mb-3">
-                  <Label>Tempat Tujuan</Label>
-                  <Creatable
-                    // value={selectedGroup}
-                    // onChange={() => {
-                    //   handleSelectGroup();
-                    // }}
-                    isMulti
-                    options={filteredDistricts}
-                    classNamePrefix="select2-selection"
-                  />
-                </div>
-              </CardBody>
-            </CardBody>
-            <CardBody className="pt-0">
-              <CardTitle>TANGGAL SURAT</CardTitle>
-              <CardBody className="p-0 px-4">
-                <InputGroup>
-                  <Flatpickr
-                    ref={datePickerRef}
-                    defaultValue="today"
-                    className="form-control d-block"
-                    placeholder="dd M, yyyy"
-                    options={{
-                      altInput: true,
-                      altFormat: "F j, Y",
-                      dateFormat: "Y-m-d",
-                    }}
-                  />
-                  <div className="input-group-append">
+          <Formik
+            initialValues={{
+              commitmentMaker: "",
+              placeOfDeparture: "",
+              destination: [],
+              dateOfLetter: new Date().toISOString(),
+            }}
+            validationSchema={validationSchema}
+            onSubmit={(values, { setSubmitting }) => {
+              const normalized = {
+                ...values,
+                placeOfDeparture: capitalizeString(values.placeOfDeparture),
+                destination: values.destination.map((item) =>
+                  capitalizeString(item)
+                ),
+              };
+              setTimeout(() => {
+                console.log(values);
+                setSubmitting(false);
+              }, 2000);
+            }}
+          >
+            {({ setFieldValue, isSubmitting, errors, touched }) => (
+              <Form>
+                <Card>
+                  <CardBody>
+                    <CardTitle>PEJABAT PEMBUAT KOMITMEN</CardTitle>
+                    <CardBody className="p-0 px-4">
+                      <div className="mb-3">
+                        <Select
+                          onChange={(option) => {
+                            console.log(option);
+                            setFieldValue("commitmentMaker", option.value);
+                          }}
+                          options={employees}
+                          classNamePrefix="select2-selection"
+                          aria-errormessage="commitmentMaker"
+                        />
+                        <Error
+                          touched={touched.commitmentMaker}
+                          message={errors.commitmentMaker}
+                        />
+                      </div>
+                    </CardBody>
+                  </CardBody>
+                  <CardBody className="pt-0">
+                    <CardTitle>KEBERANGKATAN & DESTINASI</CardTitle>
+                    <CardBody className="p-0 px-4">
+                      <div className="mb-3">
+                        <Label>Tempat Berangkat</Label>
+                        <Select
+                          // value={selectedGroup}
+                          onChange={(newValue) => {
+                            setFieldValue("placeOfDeparture", newValue.value);
+                          }}
+                          classNamePrefix="select2-selection"
+                          options={filteredDistricts}
+                        />
+                        <Error
+                          touched={touched.placeOfDeparture}
+                          message={errors.placeOfDeparture}
+                        />
+                      </div>
+                      <div className="mb-3">
+                        <Label>Tempat Tujuan</Label>
+                        <Creatable
+                          onChange={(option) => {
+                            console.log(option);
+                            setFieldValue(
+                              "destination",
+                              option.map((item) => item.value)
+                            );
+                          }}
+                          isMulti
+                          options={filteredDistricts}
+                          classNamePrefix="select2-selection"
+                        />
+                        <Error
+                          touched={touched.destination}
+                          message={errors.destination}
+                        />
+                      </div>
+                    </CardBody>
+                  </CardBody>
+                  <CardBody className="pt-0">
+                    <CardTitle>TANGGAL SURAT</CardTitle>
+                    <CardBody className="p-0 px-4">
+                      <InputGroup>
+                        <Flatpickr
+                          ref={datePickerRef}
+                          defaultValue="today"
+                          className="form-control d-block"
+                          placeholder="dd M, yyyy"
+                          options={{
+                            altInput: true,
+                            altFormat: "F j, Y",
+                            dateFormat: "Y-m-d",
+                          }}
+                        />
+                        <div>
+                          <button
+                            type="button"
+                            className="btn btn-outline-secondary docs-datepicker-trigger"
+                            onClick={() =>
+                              datePickerRef.current.flatpickr.toggle()
+                            }
+                            style={{ zIndex: 0 }}
+                          >
+                            <i className="fa fa-calendar" aria-hidden="true" />
+                          </button>
+                        </div>
+                      </InputGroup>
+                      <Error
+                        touched={touched.dateOfLetter}
+                        message={errors.dateOfLetter}
+                      />
+                    </CardBody>
+                  </CardBody>
+                </Card>
+                <Card>
+                  <CardBody>
+                    <div className="form-check mb-3">
+                      <input
+                        className="form-check-input"
+                        type="checkbox"
+                        defaultValue=""
+                        id="defaultCheck1"
+                        checked={checked}
+                        onChange={(e) => setChecked(e.target.checked)}
+                      />
+                      <label
+                        className="form-check-label"
+                        htmlFor="defaultCheck1"
+                      >
+                        Pastikan data yang anda masukan sudah benar{" "}
+                        <span className="mdi mdi-information-outline"></span>
+                      </label>
+                    </div>
                     <button
-                      type="button"
-                      className="btn btn-outline-secondary docs-datepicker-trigger"
-                      onClick={() => datePickerRef.current.flatpickr.toggle()}
+                      type="submit"
+                      className="btn btn-info btn-rounded btn-lg waves-effect waves-light"
+                      disabled={isSubmitting || !checked}
                     >
-                      <i className="fa fa-calendar" aria-hidden="true" />
+                      SIMPAN
                     </button>
-                  </div>
-                </InputGroup>
-              </CardBody>
-            </CardBody>
-          </Card>
-          <Card>
-            <CardBody>
-              <div className="form-check mb-3">
-                <input
-                  className="form-check-input"
-                  type="checkbox"
-                  defaultValue=""
-                  id="defaultCheck1"
-                />
-                <label className="form-check-label" htmlFor="defaultCheck1">
-                  Pastikan data yang anda masukan sudah benar{" "}
-                  <span className="mdi mdi-information-outline"></span>
-                </label>
-              </div>
-              <button
-                type="button"
-                className="btn btn-info btn-rounded btn-lg waves-effect waves-light"
-              >
-                SELESAI
-              </button>
-            </CardBody>
-          </Card>
+                  </CardBody>
+                </Card>
+              </Form>
+            )}
+          </Formik>
         </Container>
       </div>
     </Fragment>
   );
+};
+
+const Error = ({ touched, message }) => {
+  if (message && touched) {
+    return (
+      <div className="form-message invalid" style={{ color: "red" }}>
+        {message}
+      </div>
+    );
+  } else return null;
 };
 
 export default EditSuratPerjalananDinas;
